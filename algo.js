c6f83c59455eb0bdfe492baeebb26d85ef08e733
auzias <github@auzias.net>
2015-03-30 16:16:34 +0200

    aes decryption?! What are these results??

--- algo.js
+++ algo.js
@@ -1,20 +1,35 @@
-
-function md5pass_salt_update(){
-  document.getElementById('md5salt').innerHTML = document.getElementById('md5pass-salt').value;
-  md5pass_update();
-}
-function md5pass_update(){
-  document.getElementById('md5hash').innerHTML = document.getElementById('md5pass-pass').value;
-}
-
-function update(){
+function update_no_salt() {
   var stringToEncode = document.getElementById('string').value;
   document.getElementById('base64_encoded').innerHTML = window.btoa(stringToEncode);
-  document.getElementById('md5_summed').innerHTML  = CryptoJS.MD5(stringToEncode);
-  document.getElementById('sha1_hash').innerHTML   = CryptoJS.SHA1(stringToEncode);
-  document.getElementById('sha2_hash').innerHTML   = CryptoJS.SHA256(stringToEncode);
-  document.getElementById('sha512_hash').innerHTML = CryptoJS.SHA512(stringToEncode);
-  document.getElementById('sha3_hash').innerHTML   = CryptoJS.SHA3(stringToEncode);
-  var ripemd = document.getElementById('ripemd').innerHTML   = CryptoJS.RIPEMD160(stringToEncode);
-  document.getElementById('ripemd64').innerHTML   = ripemd.toString(CryptoJS.enc.Latin1);
+  document.getElementById('md5_summed').innerHTML  =  CryptoJS.MD5(stringToEncode);
+  document.getElementById('sha1_hash').innerHTML   =  CryptoJS.SHA1(stringToEncode);
+  document.getElementById('sha2_hash').innerHTML   =  CryptoJS.SHA256(stringToEncode);
+  document.getElementById('sha512_hash').innerHTML =  CryptoJS.SHA512(stringToEncode);
+  document.getElementById('sha3_hash').innerHTML   =  CryptoJS.SHA3(stringToEncode);
+  document.getElementById('ripemd').innerHTML      =  CryptoJS.RIPEMD160(stringToEncode);
+}
+
+function update_msg() {
+  var msg  = document.getElementById('msg').value;
+  var pass = document.getElementById('pass').value;
+  document.getElementById('hmac_md5').innerHTML    = CryptoJS.HmacMD5(msg, pass);
+  document.getElementById('hmac_sha1').innerHTML   = CryptoJS.HmacSHA1(msg, pass);
+  document.getElementById('hmac_sha256').innerHTML = CryptoJS.HmacSHA256(msg, pass);
+  document.getElementById('hmac_sha512').innerHTML = CryptoJS.HmacSHA512(msg, pass);
+  document.getElementById('hmac_sha3').innerHTML   = CryptoJS.HmacSHA3(msg, pass);
+  document.getElementById('aes').innerHTML   = CryptoJS.AES.encrypt(msg, pass);
+}
+
+function update_salt() {
+  var pssp = document.getElementById('pssp').value;
+  var salt = document.getElementById('salt').value;
+  document.getElementById('pbkdf').innerHTML = CryptoJS.HmacMD5(pssp, salt, { keySize: 128/32});
+
+}
+
+function d_update_msg() {
+  var d_msg  = document.getElementById('d_msg').value;
+  var d_pass = document.getElementById('d_pass').value;
+  document.getElementById('d_aes').innerHTML   = CryptoJS.AES.decrypt(d_msg, d_pass);
+
 }
