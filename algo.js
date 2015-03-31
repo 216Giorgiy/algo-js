3b3188243e4f087dc88be3de99b1af9d9e566146
auzias <github@auzias.net>
2015-03-31 16:45:09 +0200

    update to correct all "onkeyup"

--- algo.js
+++ algo.js
@@ -21,18 +21,11 @@ function update_msg() {
   document.getElementById('hmac_sha512').innerHTML = CryptoJS.HmacSHA512(msg, pass);
   document.getElementById('hmac_sha3').innerHTML   = CryptoJS.HmacSHA3(msg, pass);
 */
-  document.getElementById('aes').innerHTML         = CryptoJS.AES.encrypt(msg, pass);
+  document.getElementById('aes').innerHTML         = CryptoJS.AES.encrypt("msg", "pass");
   document.getElementById('des').innerHTML         = CryptoJS.DES.encrypt(msg, pass);
   document.getElementById('t-des').innerHTML       = CryptoJS.TripleDES.encrypt(msg, pass);
   document.getElementById('rbbt').innerHTML        = CryptoJS.Rabbit.encrypt(msg, pass);
   document.getElementById('rc4').innerHTML         = CryptoJS.RC4Drop.encrypt(msg, pass);
-}
-
-function update_salt() {
-  var pssp = document.getElementById('pssp').value;
-  var salt = document.getElementById('salt').value;
-  document.getElementById('pbkdf').innerHTML = CryptoJS.HmacMD5(pssp, salt, { keySize: 128/32});
-
 }
 
 function d_update_msg() {
@@ -42,8 +35,13 @@ function d_update_msg() {
   document.getElementById('d_des').innerHTML   = hex2a(CryptoJS.DES.decrypt(d_msg, d_pass));
   document.getElementById('d_t-des').innerHTML = hex2a(CryptoJS.TripleDES.decrypt(d_msg, d_pass));
   document.getElementById('d_rbbt').innerHTML  = hex2a(CryptoJS.Rabbit.decrypt(d_msg, d_pass));
-  document.getElementById('d_rc4').innerHTML  = hex2a(CryptoJS.RC4Drop.decrypt(d_msg, d_pass));
+  document.getElementById('d_rc4').innerHTML   = hex2a(CryptoJS.RC4Drop.decrypt(d_msg, d_pass));
+}
 
+function update_salt() {
+  var pssp = document.getElementById('pssp').value;
+  var salt = document.getElementById('salt').value;
+  document.getElementById('pbkdf').innerHTML = CryptoJS.HmacMD5(pssp, salt, { keySize: 128/32});
 }
 
 function hex2a(hexx) {
